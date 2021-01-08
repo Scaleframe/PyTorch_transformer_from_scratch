3db7aa2aa4fcf0dc75760005b609b12b9ef7ddcc
Jim Devine <jiim.devine@gmail.com>
2021-01-08 15:02:43 -0500

    move self attention to class, add init

--- self_attention.py
+++ self_attention.py
@@ -1,22 +1,9 @@
 import torch 
+from torch import nn
 import torch.nn.functional as F 
 
-# assume we have some tensor x with size (b, t, k)
-x = ...
 
-
-raw_weights = torch.bmm(x, x.transpose(1,2))
-    # torch.bmm is batched matmul. 
-
-# our goal is to get a weighted sum over all our embedding vectors in the input sequence. 
-
-weights = F.softmax(raw_weights, dim=2)
-    # this gives us values that sum to one
-
-
-# comput the output sequence
-y = torch.bmm(weights, x)
-
-# two matrix muliplications and one softmax gives us self attention. 
-
+class SelfAttention(nn.Module):
+    def __init__(self, k, head=8):
+        pass
 
